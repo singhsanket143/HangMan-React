f7857ef58b4c3c961e5edc528d64377e8fccb1c2
Anas <anasansari2357@gmail.com>
2024-07-27 12:09:42 +0530

    added vissibility change for password input

--- src/components/TextInput/TextInput.jsx
+++ src/components/TextInput/TextInput.jsx
@@ -1,19 +1,32 @@
-function TextInput({ label, type = 'text', value, onChange }) {
+import { useState } from "react";
+import Button from "../Button/Button";
+
+function TextInput({ label, type = "text", value, onChange }) {
+    const [visibility, setVisibility] = useState(type);
+
+    function handleClick() {
+        setVisibility(prevVisibility => (prevVisibility === "password" ? "text" : "password"));
+    }
 
     return (
-
         <label className="block">
             {label && <span className="text-gray-700">{label}</span>}
             <input
                 className="px-4 py-2 border border-gray-500 rounded-md w-full"
                 onChange={onChange}
                 placeholder={label}
-                type={type}
+                type={visibility}
                 value={value}
             />
-
+            {type === "password" && (
+                <Button 
+                    text="text"
+                    type="button"
+                    onClickHandler={handleClick}
+                />
+            )}
         </label>
     );
-
 }
+
 export default TextInput;
