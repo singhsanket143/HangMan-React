0c311f134d5d5e5e1d76c43d7920ac35a8464204
aditya bhojane <adityabhojane66@gmail.com>
2024-07-27 12:03:21 +0530

    add new feature

--- src/components/TextInputForm/TextInputForm.jsx
+++ src/components/TextInputForm/TextInputForm.jsx
@@ -3,7 +3,7 @@ import Button from "../Button/Button" ;
 import { useState } from "react";
 
 function TextInputForm({ onSubmit }) {
-
+    const [type,setType] = useState('password')
     const [value, setValue] = useState('');
 
     function handleFormSubmit(event) {
@@ -17,18 +17,28 @@ function TextInputForm({ onSubmit }) {
         console.log(event.target.value);
         setValue(event.target.value); // whenever I Type in the input field, it will update the value
     }
+    let toggle = false;
+    function ShowPass(){
+        if(toggle){
+            setType('password');
+            toggle = false;
+        }else{
+            setType('text');
+            toggle = true;
+        }
+    }
 
     return (
         <form className="flex" onSubmit={handleFormSubmit}>
             <div className="mr-2 flex-1">
                  <TextInput 
                     label="Enter a word or phrase"
-                    type="password"
+                    type={type}
                     value={value}
                     onChange={handleTextInputChange}
                 />
             </div>
-
+            <button onClick={ShowPass}><span>show pass</span></button>
             <div className="flex">
                     <Button 
                         text="OK"
