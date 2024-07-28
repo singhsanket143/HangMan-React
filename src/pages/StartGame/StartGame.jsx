84c64028a44d050a119ddc6a42580caa5fbd7399
Sanket Singh <singhsanket143@gmail.com>
2024-07-28 12:52:33 +0530

    Added react router dom support

--- src/pages/StartGame/StartGame.jsx
+++ src/pages/StartGame/StartGame.jsx
@@ -1,11 +1,18 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import TextInputFormContainer from "../../components/TextInputForm/TextInputFormContainer";
 
 function StartGame() {
+
+    const navigate = useNavigate();
+
+    function handleSubmit() {
+        navigate('/play');
+    }
 
     return (
         <div>
             <h1>Start Game</h1>
-            <Link to='/play'> play Game </Link>
+            <TextInputFormContainer onSubmit={handleSubmit} />
         </div>
     );
 }
